8b90f9d27cec97e3bfaef8849eee06a7f379e3a6
Anurag <danurag1906@gmail.com>
2023-01-05 14:22:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import PDFMerger from "pdf-merger-js/browser";
+import "./App.css";
+import {MdPictureAsPdf} from'react-icons/md'
 
 function App() {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [mergedPdfUrl, setMergedPdfUrl] = useState();
+
+  useEffect(() => {
+    const render = async () => {
+      const merger = new PDFMerger();
+
+      for (const file of uploadedFiles) {
+        await merger.add(file);
+      }
+
+      const mergedPdf = await merger.saveAsBlob();
+      const url = URL.createObjectURL(mergedPdf);
+
+      return setMergedPdfUrl(url);
+    };
+
+    render().catch((err) => {
+      throw err;
+    });
+
+     setMergedPdfUrl();
+  }, [uploadedFiles, setMergedPdfUrl]);
+
+  const handleUploadFiles = (files) => {
+    const uploaded = [...uploadedFiles];
+    files.forEach((file) => {
+      uploaded.push(file);
+    });
+    setUploadedFiles(uploaded);
+  };
+
+  const handleFileEvent = (e) => {
+    const chosenFiles = Array.prototype.slice.call(e.target.files);
+    handleUploadFiles(chosenFiles);
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        style={{ display: "none" }}
+        id="fileUpload"
+        type="file"
+        accept="application/pdf"
+        multiple
+        onChange={handleFileEvent}
+      />
+      <label htmlFor="fileUpload">
+        <a className="btn btn-primary my-2"> Upload Files </a>
+      </label>
+
+      <a className="btn btn-warning my-2" style={{marginLeft:'1rem'}} href={`${mergedPdfUrl}`} download >Merge Files</a>
+      
+
+      {!mergedPdfUrl?(
+        <p className="danger" >only pdf files allowed</p>
+      ):(
+        <div className="uploaded-files-list">
+        {uploadedFiles.map((file) => (
+          <div className="eachFile" > <MdPictureAsPdf/> {file.name}</div>
+        ))}
+      </div>
+      )}
+
     </div>
   );
 }
