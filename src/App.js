8035637df5dfa515c7281e6744a5ad50a328ee32
Anurag <danurag1906@gmail.com>
2023-01-06 15:49:57 +0530

    implemented file order swapping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import PDFMerger from "pdf-merger-js/browser";
 import "./App.css";
 import {MdPictureAsPdf} from'react-icons/md'
+import {BsFillArrowDownSquareFill,BsFillArrowUpSquareFill} from 'react-icons/bs'
+import {FaArrowCircleRight} from 'react-icons/fa'
 
 function App() {
   const [uploadedFiles, setUploadedFiles] = useState([]);
@@ -41,10 +43,51 @@ function App() {
     handleUploadFiles(chosenFiles);
   };
 
+  const handleUp=(name)=>{
+    const data=[...uploadedFiles]
+    let index;
+    
+    if(name!=data[0].name){
+      for(let i=0;i<data.length;i++){
+        if(data[i].name==name){
+          index=i;
+          break;
+        }
+      }
+      let temp=data[index];
+      data[index]=data[index-1];
+      data[index-1]=temp;
+      setUploadedFiles(data)
+    }
+  }
+
+  const handleDown=(name)=>{
+    const data=[...uploadedFiles]
+    let index;
+    
+    if(name!=data[data.length-1].name){
+      for(let i=0;i<data.length;i++){
+        if(data[i].name==name){
+          index=i;
+          break;
+        }
+      }
+
+      [data[index],data[index+1]]=[data[index+1],data[index]]
+      setUploadedFiles(data)
+    }
+  }
   
 
   return (
     <div className="App">
+      <div className="header">
+      <h4 className="mb-4 p-2" >PDF-Merger</h4>
+      </div>
+      <div className="instructions">
+        <p><FaArrowCircleRight/> Add the PDF files in the order to be merged</p>
+        <p><FaArrowCircleRight/> Refresh to clear data</p>
+      </div>
       <input
         style={{ display: "none" }}
         id="fileUpload"
@@ -53,19 +96,21 @@ function App() {
         multiple
         onChange={handleFileEvent}
       />
-      <label htmlFor="fileUpload">
-        <a className="btn btn-primary my-2"> Upload Files </a>
-      </label>
+      <div className="buttons">
+        <label htmlFor="fileUpload">
+          <a className="btn btn-danger my-2"> Upload Files </a>
+        </label>
 
-      <a className="btn btn-warning my-2" style={{marginLeft:'1rem'}} href={`${mergedPdfUrl}`} download >Merge Files</a>
+        <a className="btn btn-warning my-2 mx-2"  href={`${mergedPdfUrl}`} download >Merge Files</a>
+      </div>
       
 
       {!mergedPdfUrl?(
-        <p className="danger" >only pdf files allowed</p>
+        <p className="danger" >Loading...</p>
       ):(
         <div className="uploaded-files-list">
         {uploadedFiles.map((file) => (
-          <div className="eachFile" > <MdPictureAsPdf/> {file.name}</div>
+          <div className="eachFile" > <MdPictureAsPdf/> {file.name} <div className="arrows"><BsFillArrowUpSquareFill onClick={()=>handleUp(file.name)} /><BsFillArrowDownSquareFill onClick={()=>handleDown(file.name)} /></div> </div>
         ))}
       </div>
       )}
